b81db5d246419c9e4caa2fa6b03a5053d7b9224d
jiangtao <jiangtao.jit@gmail.com>
2016-07-28 18:02:51 +0800

    context

--- app/src/main/java/com/tao/jiang/mytestapp/MainActivity.java
+++ app/src/main/java/com/tao/jiang/mytestapp/MainActivity.java
@@ -1,13 +1,23 @@
 package com.tao.jiang.mytestapp;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Activity activity;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        activity = this;
+        context = this;
+
+
         setContentView(R.layout.activity_main);
+
     }
 }
